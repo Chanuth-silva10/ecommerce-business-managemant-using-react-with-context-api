ee0e681a2eeb3b2a9df2667eaf57122813fa72fc
Chanuth10 <it20074418@my.sliit.lk>
2022-01-30 21:59:51 +0530

    Testing Api For userRole

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -1,7 +1,22 @@
+const Category = require('../models/categoryModel')
+
 const categoryCtrl = {
     getCategories : async (req, res) => {
-     res.json('category api testing')
-    }
-}
+         try{
+             const categories = await Category.find()
+             res.json(categories)
+         }catch(err){
+             return res.status(500).json({msg: err.message})
+         }
+    },
+    createCategory : async (req, res) =>{
+         try{
+             res.json('Check admin success')
+         }catch(err){
+             return res.status(500).json({msg: err.message})
+         }
+        },
+ }  
+
 
 module.exports = categoryCtrl
