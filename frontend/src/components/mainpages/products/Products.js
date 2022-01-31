0b0d39a16a5b360a15e9dd1c8e071dbb36dfd3c1
Chanuth10 <it20074418@my.sliit.lk>
2022-01-31 19:45:11 +0530

    Get product details from GlobalState

--- frontend/src/components/mainpages/products/Products.js
+++ frontend/src/components/mainpages/products/Products.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useContext, useState} from 'react'
+import {GlobalState} from '../../../GlobalState'
+
 
 function Products() {
+    const state = useContext(GlobalState)
+
+    console.log(state)
   return (
       <div>
           Product Lists
