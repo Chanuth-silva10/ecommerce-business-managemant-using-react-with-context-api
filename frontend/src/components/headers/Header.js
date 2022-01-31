0a5dbd158b76f0c30593c508ad9151fe342f7c16
Chanuth10 <it20074418@my.sliit.lk>
2022-01-31 17:30:01 +0530

    Create Menu

--- frontend/src/components/headers/Header.js
+++ frontend/src/components/headers/Header.js
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react';
 import {GlobalState} from '../../GlobalState'
+import Menu from './icon/menu.svg'
+import Close from './icon/close.svg'
+import Cart from './icon/cart.svg'
 
 function Header() {
-    const value = useContext(GlobalState)
-  return <div>
-      {value}
-  </div>;
+  const value = useContext(GlobalState)
+  return(
+   <header>
+        <div className='menu'>
+             <img src={Menu} alt=""  width="30"/>
+        </div>
+  </header>
+  );
 }
 
 export default Header;
