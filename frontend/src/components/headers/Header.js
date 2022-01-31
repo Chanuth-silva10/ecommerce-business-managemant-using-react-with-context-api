56402d3c69992808d26337fc4927b65682fef53b
Chanuth10 <it20074418@my.sliit.lk>
2022-01-31 18:15:06 +0530

    Implement header

--- frontend/src/components/headers/Header.js
+++ frontend/src/components/headers/Header.js
@@ -3,6 +3,7 @@ import {GlobalState} from '../../GlobalState'
 import Menu from './icon/menu.svg'
 import Close from './icon/close.svg'
 import Cart from './icon/cart.svg'
+import { Link } from 'react-router-dom';
 
 function Header() {
   const value = useContext(GlobalState)
@@ -10,6 +11,27 @@ function Header() {
    <header>
         <div className='menu'>
              <img src={Menu} alt=""  width="30"/>
+        </div>
+        <div className='logo'>
+             <h1>
+                 <Link to="/">MagCity Shop</Link>
+             </h1>
+        </div>
+
+        <ul>
+            <li><Link to="/">Products</Link></li>
+            <li><Link to="/">Login & Register</Link></li>
+
+            <li>
+                <img src={Close} alt='' width="30"></img>
+            </li>
+        </ul>
+
+        <div className='cart-icon'>
+             <span>0</span>
+             <Link to="/cart">
+                 <img src={Cart} alt='' width="30"></img>
+             </Link>
         </div>
   </header>
   );
