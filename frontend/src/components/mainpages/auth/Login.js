19fd43f6298d1ef2b5159da68554b9bacbf95d1e
Chanuth10 <it20074418@my.sliit.lk>
2022-01-31 18:28:25 +0530

    Go to Components

---
+++ frontend/src/components/mainpages/auth/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login() {
+  return (
+  <div>
+      Login Components
+  </div>
+  )
+}
+
+export default Login;
---
+++ frontend/src/components/mainpages/products/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Products() {
+  return (
+      <div>
+          Product Lists
+      </div>
+  )
+}
+
+export default Products;
