8fead9ee3383786cc6fb7c8642d4427e0fcd715c
Chanuth10 <it20074418@my.sliit.lk>
2022-02-03 09:45:06 +0530

    Get relevent produc id(testing)

--- frontend/src/components/mainpages/detailProduct/DetailProduct.js
+++ frontend/src/components/mainpages/detailProduct/DetailProduct.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useContext, useState, useEffect} from 'react';
+import {useParams, Link} from 'react-router-dom'
+import {GlobalState} from '../../../GlobalState'
 
 function DetailProduct() {
+  const params = useParams()
+  console.log(params)
   return(
       <div>
           Details Pro
