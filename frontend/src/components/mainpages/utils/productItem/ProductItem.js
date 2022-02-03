5f56c7acfc6ad469772e1b14769e09ae62340ee9
Chanuth10 <it20074418@my.sliit.lk>
2022-02-03 09:24:44 +0530

    Adding css productItem page and product page

--- frontend/src/components/mainpages/utils/productItem/ProductItem.js
+++ frontend/src/components/mainpages/utils/productItem/ProductItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BtnRender from './BtnRender'
+import {Link} from 'react-router-dom'
 
 function ProductItem({product}) {
     console.log(product)
@@ -10,12 +11,21 @@ function ProductItem({product}) {
 
             <div className="product_box">
                 <h2 title={product.title}>{product.title}</h2>
-                <span>$ {product.price}</span>
+                <span>${product.price}</span>
                 <p>{product.description}</p>
             </div>
 
+            <div className='row_btn'>
+                 <Link id="btn_buy" to="#!">
+                     Buy
+                 </Link>
+                  <Link id="btn_view" to={`detail/${product._id}`}>
+                     view
+                 </Link>
+            </div>
+
         </div>
-    )
+    )   
 }
 
 export default ProductItem
